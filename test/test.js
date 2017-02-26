9db20b5ad20098cca767724de7e3c54ca9d4e65e
Ashish Kumar <ashish.103079@gmail.com>
2017-02-27 01:13:22 +0530

    Added pass and fail both

--- test/test.js
+++ test/test.js
@@ -4,13 +4,25 @@ var expect = require("chai").expect;
 var baseUrl = "https://swapi.co/api";
 var util = require("util");
 
-describe('returns Ashish', function() {
-    it('returns luke', function(done) {
+describe('Sample Test Suit', function() {
+    it('Pass Test', function(done) {
         request.get({ url: baseUrl + '/people/1/' },
             function(error, response, body) {
             		var bodyObj = JSON.parse(body);
             		expect(bodyObj.name).to.equal("Luke Skywalker");
             		expect(bodyObj.hair_color).to.equal("blond");
+                    expect(response.statusCode).to.equal(200);
+                    console.log(body);
+                done();
+            });
+    });
+    // Hair colour changed from blond to black
+     it('Fail Test', function(done) {
+        request.get({ url: baseUrl + '/people/1/' },
+            function(error, response, body) {
+            		var bodyObj = JSON.parse(body);
+            		expect(bodyObj.name).to.equal("Luke Skywalker");
+            		expect(bodyObj.hair_color).to.equal("black");
                     expect(response.statusCode).to.equal(200);
                     console.log(body);
                 done();
